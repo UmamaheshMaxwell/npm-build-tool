0a536d1db570bcfa6a187b00236bb753a0655844
UmamaheshMaxwell <maheshmeka16@gmail.com>
2017-05-21 19:50:32 +0530

    Adds lint for required js files

--- test/testingTypescriptCode.js
+++ test/testingTypescriptCode.js
@@ -4,6 +4,6 @@ const greeter = require("../lib/tsCode.js");
 describe("Test suite for typescript", function(){
 	it("should greet the user", function(){
 		var greeting = new greeter();
-		greeting.greet("Vani").should.equal("A type-scripting greeting to you, Vani")
-	})
-})
+		greeting.greet("Vani").should.equal("A type-scripting greeting to you, Vani");
+	});
+});
