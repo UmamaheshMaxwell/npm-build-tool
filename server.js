f667dc79c294c4803c5077159bfe17638eedfd22
UmamaheshMaxwell <maheshmeka16@gmail.com>
2017-05-21 20:35:02 +0530

    Adds html using swig

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const koa = require("koa");
+const path = require("path");
+const swig  = require('swig');
 const app = new koa();
+
+var template = swig.compileFile('./index.html');
+
 app.use(context => {
-	context.body = "koa Says Hello";
+	context.body = template();
 });
 
 const PORT = process.env.PORT || (process.argv[2] || 1337);
